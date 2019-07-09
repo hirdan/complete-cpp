ab6cfb3afc6f964ebe3d88c5df2db9e7d58361b4
Hirdan <aggarwalhirdan@gmail.com>
2019-07-09 11:29:44 +0530

    cpp

---
+++ data_types/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void main() //main is a function of data type void that doesn't return anything
+{
+    int num = 10; //num stores an integer
+    char alpha = 'a'; //alpha stores a character. note the quotes.
+    float pi = 3.14;
+    // pi is a floating point variable that stores value 3.14
+    double h = 6.626e-34
+    //floating point numbers can also be written in scientific notation
+    //6.626e-34 = 6.626 * 10^-34
+    bool isPrime =  true;
+    //isPrime is a var CAN store just 2 values either 0 or 1
+}
