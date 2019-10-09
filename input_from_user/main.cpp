4c98150cd4e4b4a5499c51178fb252409ad5e1f1
sukirt01 <54432666+sukirt01@users.noreply.github.com>
2019-10-09 13:08:12 +0530

    Update main.cpp

--- input_from_user/main.cpp
+++ input_from_user/main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-    int x;
-    cout << "Enter Value for x:";
-    cin >> x;//read into x
+    int y;
+    cout << "Enter Value for y:";
+    cin >> y;//read into y
 }
