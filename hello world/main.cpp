14fa01d2a4fc5a8931115a670d83e0950930f458
Hirdan Aggarwal <aggarwalhirdan@gmail.com>
2021-10-01 07:59:47 +0530

    Update main.cpp

--- hello world/main.cpp
+++ hello world/main.cpp
@@ -1,10 +1,11 @@
-#include <iostream>
-
+#include <bits/stdc++.h>
 using namespace std;
 
 int main()
 {   //print a string
-    cout << "Hello to coding world!";
+    cout << "Hello to the best coding world!";
+    cout<<endl;
+    cout << "Edit by Hirdan Aggarwal!";
     //print new line
     cout<<endl;
 }
