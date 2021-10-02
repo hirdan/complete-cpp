ca45b08ab35738b456267ea2d86dc277bd268fe3
Hirdan Aggarwal <aggarwalhirdan@gmail.com>
2021-10-02 08:01:01 +0530

    Update main.cpp

--- data_types/main.cpp
+++ data_types/main.cpp
@@ -8,6 +8,10 @@ void main() //main is a function of data type void that doesn't return anything
     char alpha = 'a'; //alpha stores a character. note the quotes.
     float pi = 3.14;
     // pi is a floating point variable that stores value 3.14
+    int num1 = 20; //num1 stores an integer
+    char alpha1 = 'b'; //alpha1 stores a character. note the quotes.
+    float pi1 = 3.142;
+    // pi1 is a floating point variable that stores value 3.14
     double h = 6.626e-34
     //floating point numbers can also be written in scientific notation
     //6.626e-34 = 6.626 * 10^-34
