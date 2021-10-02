57231a5577692e2e597d2b1c7f2772cbb33bbbc3
Hirdan Aggarwal <aggarwalhirdan@gmail.com>
2021-10-02 08:02:36 +0530

    Update main.cpp

--- input_from_user/main.cpp
+++ input_from_user/main.cpp
@@ -5,6 +5,9 @@ using namespace std;
 int main()
 {
 
+    int x;
+    cout << "Enter Value for x:";
+    cin >> x;//read into x
     int y;
     cout << "Enter Value for y:";
     cin >> y;//read into y
